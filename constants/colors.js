40780573849ba293cceab67d4a69f046ba0df8e1
GyabaahFelix <felixic360@gmail.com>
2026-08-05 18:33:44 +0000

    Finalize workout app configuration

---
+++ constants/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  background: "#FFF7F8",
+  card: "#FFFFFF",
+  primary: "#FF5B78",
+  primaryLight: "#FFE5EA",
+  burgundy: "#5A2733",
+  text: "#211A1C",
+  secondaryText: "#756B6E",
+  border: "#F1DDE1",
+  favourite: "#FF3F65",
+};
+
+export default colors;
